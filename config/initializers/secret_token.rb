2834a9bbf24b2d3c7a8b426688e6a7e3450e7e4e
Marvin Mante <marvinsjsu@gmail.com>
2014-06-22 22:17:10 -0700

    Building Blogger App from JumpStartLab tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'dc3c5a6e62870d90c18032985d9f059f681ff722cf5afeb7a67547092123e0d572fcccac704dd059635cb96a5f772028b9a4c5dc4cb2c2961a08fd4817be1e26'
